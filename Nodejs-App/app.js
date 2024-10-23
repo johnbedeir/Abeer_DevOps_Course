92e154a8c82a1c8dca8c4729f2241c9c1f3dbb0d
johnbedeir <john.bedeir@gmail.com>
2024-10-23 11:05:35 -0400

    Nodejs app

---
+++ Nodejs-App/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define a route for '/'
+app.get('/', (req, res) => {
+  res.send('Hello, Docker with Node.js!');
+});
+
+// Start the server
+const port = 5000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App running on http://0.0.0.0:${port}`);
+});
